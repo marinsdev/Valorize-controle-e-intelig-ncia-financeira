f49c14868f951646b2642a2fc0bc6e029ba06407
marinsdev <marinsgisele@outlook.com>
2025-12-13 15:57:52 -0300

    teste

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Gisele Marins de Oliveira on 13/12/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
